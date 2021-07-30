c8e62bbd6f6185df7342824e07e0c4535fcc1d62
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-07-30 14:07:16 -0500

    Create model Todo

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    task = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
