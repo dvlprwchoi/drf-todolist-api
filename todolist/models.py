33db0df1be9e050eb7efd0cf9535b0cd252df5e2
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-07-30 14:32:05 -0500

    Change models

--- todolist/models.py
+++ todolist/models.py
@@ -7,7 +7,22 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     place = models.CharField(max_length=100)
     task = models.TextField()
+    author = models.ForeignKey(
+        'users.User', related_name='todolist', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    todo = models.ForeignKey(
+        Todo, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        'users.User', related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
